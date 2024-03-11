dbbfad4b07679242b847e74619c3cbb3b074367f
Trenton H <797416+stumpylog@users.noreply.github.com>
2024-03-11 16:13:28 -0700

    Latest packages, adds OpenAPI generator

--- src-frontend/src/app/app.routes.ts
+++ src-frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { PeopleComponent } from './people/people.component';
+
+export const routes: Routes = [
+  {
+    path: 'people',
+    component: PeopleComponent,
+  },
+];
