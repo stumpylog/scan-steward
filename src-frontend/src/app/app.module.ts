165c7bb95aa8b005d84f020a32d48d47eb0c752c
Trenton H <797416+stumpylog@users.noreply.github.com>
2024-03-19 19:51:40 -0700

    Re-created fresh project

--- src-frontend/src/app/app.module.ts
+++ src-frontend/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
